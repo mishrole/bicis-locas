57aac84f649918bca18e34ce077f17555457bef3
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-02 18:20:55 -0500

    primer commit

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,3 +1,32 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+
+	var nombre = document.getElementById("name");
+	var apellido = document.getElementById("lastname");
+	var correo = document.getElementById("input-email");
+	var contra = document.getElementById("input-password");
+
+	var regLetras = /^[a-zA-Z]*$/;
+	var regNumeros;
+	/*
+
+	if(nombre.value.length == 0){
+		alert("Escribe tu nombre")
+	}else if(apellido.value.length == 0){
+		alert("Escribe tu apellido")
+	}
+
+	*/
+
+	if(nombre.value.search(regLetras) || apellido.value.search(regLetras)){
+		alert("Ingresa sólo letras");
+	}
+
+	if(contra.value.length < 6){
+		alert("La contraseña debe tener al menos 6 caracteres");
+	}
+
+
+
+
+
 }
