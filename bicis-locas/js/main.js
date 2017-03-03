6b12b08360a3024d71cbbfd048d77827be8549ef
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-03 11:34:58 -0500

    Validaciones completas. Campos obligatorios completos con excepción de Camel Case

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -4,21 +4,32 @@ function validateForm(){
 	var apellido = document.getElementById("lastname");
 	var correo = document.getElementById("input-email");
 	var contra = document.getElementById("input-password");
+	var eleccion = document.getElementById("opciones")
+	var eleccionVacia = eleccion.options[eleccion.selectedIndex].value
 
 	var regLetras = /^[a-zA-Z]*$/;
-	/*
+
+	//Campos obligatorios
 
 	if(nombre.value.length == 0){
-		alert("Escribe tu nombre")
+		alert("Escribe un nombre")
 	}else if(apellido.value.length == 0){
-		alert("Escribe tu apellido")
+		alert("Escribe un apellido")
+	}else if(correo.value.length == 0){
+		alert("Escribe un e-mail")
+	}else if(contra.value.length == 0){
+		alert("Escribe una contraseña")
 	}
 
-	*/
+	if(eleccionVacia == "0"){
+		alert("Seleccione una opción")
+	}
+
+	//Campos válidos
 
 	if(nombre.value.search(regLetras) || apellido.value.search(regLetras)){
 		alert("Ingresa sólo letras");
-	}else if(contra.value.length < 6){
+	}else if(contra.value.length > 1 && contra.value.length < 6){
 		alert("La contraseña debe tener al menos 6 caracteres");
 	}else{
 		if((contra.value == "password" || contra.value == "123456") || contra.value == "098765"){
